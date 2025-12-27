daa370052933edca96682d3d249ff7658925c488
Sai Balusulapalem <balusulapalem.saikoushik@gmail.com>
2025-12-26 22:59:03 -0600

    change version text and color for beta

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -29,7 +29,7 @@ export default function NavBar({ username }: { username?: string }) {
             md:h-auto
             md:overflow-visible`
         }>
-            <Link className="navbar-item" href="/">ramrs <Version text="experimental" /></Link>
+            <Link className="navbar-item" href="/">ramrs <Version text="beta" /></Link>
             <button className="p-5 cursor-pointer md:hidden ml-auto" onClick={expand}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
